f319aadb5acffefe6e9a51d4073c356cc3b050ff
Andrew <andyburbank@gmail.com>
2022-05-23 19:04:57 +0800

    Configure the application

---
+++ app/ext.py
@@ -0,0 +1,12 @@
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+model = SQLAlchemy()
+migrate = Migrate()
+loginManager = LoginManager()
+
+def init_ext(app):
+    model.init_app(app=app)
+    migrate.init_app(app=app)
+    loginManager.init_app(app=app)
