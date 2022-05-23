e99cf1e17e35e61ccfa082348572bddab00cb74d
Andrew <andyburbank@gmail.com>
2022-05-23 21:02:06 +0800

    util

---
+++ app/utils.py
@@ -0,0 +1,17 @@
+from app.ext import model
+class BaseModel:
+    def save(self):
+        try:
+            model.session.add(self)
+            model.session.commit()
+            return 'success'
+        except Exception as e:
+           return 'error' + str(e)
+
+    def delete(self):
+        try:
+            model.session.add(self)
+            model.session.commit()
+            return 'success'
+        except Exception as e:
+            return 'error' + str(e)
